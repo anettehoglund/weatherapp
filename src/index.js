f767c7ce9331e196d4b8fb383868d30454648eb8
anettehoglund <hoglundanette@gmail.com>
2024-02-26 22:48:48 +0100

    added current weatherdetails

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 function refreshTemp(response){
     let currentTemperature = document.querySelector("#current-temp");
     let temperature = response.data.temperature.current;
-        let newCity = document.querySelector("#weatherapp-city");
+    let newCity = document.querySelector("#weatherapp-city");
+        let cityDetail = document.querySelector("#details");
+        let cityHumidity = document.querySelector("#humidity");
+        let cityWind = document.querySelector("#wind");
+        let cityTime = document.querySelector("#time");
+        let date = new Date(response.data.time * 1000);
+
+    console.log(response.data);
     
     newCity.innerHTML = response.data.city;
     currentTemperature.innerHTML = Math.round(temperature);
+        cityHumidity.innerHTML = `${response.data.temperature.humidity}%`;
+        cityDetail.innerHTML = response.data.condition.description;
+        cityWind.innerHTML = `${response.data.wind.speed}km/h`;
+        cityTime.innerHTML = formatDate(date);
+}
+
+function formatDate(date){
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hour}:${minutes}`;
 }
 
 function searchCityInput (newCity){
